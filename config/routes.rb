924c4005fb4c0d7de5447b04c8a78144e2bf5ce4
Joseph DiFrancesco <joseph.difrancesco@gmail.com>
2017-06-13 14:02:49 +0200

    just created appointments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
 
 
+  get 'appointments/index'
+
+  get 'appointments/show'
+
+  get 'appointments/new'
+
+  get 'appointments/create'
+
+  get 'appointments/edit'
+
+  get 'appointments/update'
+
+  get 'appointments/destroy'
+
   devise_for :users
 
   get "homepage" , to: "pages#home"
