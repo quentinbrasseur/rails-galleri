009c9fb6702749235812d701408cfd864a5e92a0
kilianberlichingen <kilian39@hotmail.de>
2017-06-13 16:38:26 +0200

    ugly navbar, but still navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170613111849) do
 
   # These are extensions that must be enabled in order to support this database
@@ -43,7 +42,6 @@ ActiveRecord::Schema.define(version: 20170613111849) do
     t.string   "email",                  default: "", null: false
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "phone_number"
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -60,6 +58,7 @@ ActiveRecord::Schema.define(version: 20170613111849) do
     t.string   "last_name"
     t.string   "token"
     t.datetime "token_expiry"
+    t.string   "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
