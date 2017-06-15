f3671a0d44160193e1898891d4881a09f3a0cf74
Joseph DiFrancesco <joseph.difrancesco@gmail.com>
2017-06-15 16:08:53 +0200

    search functionality is working

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,66 +1,112 @@
 class ArtworksController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show, :index]
+  skip_before_action :authenticate_user!, only: [:show, :index,:search]
 
   def index
-    @artworks = Artwork.all
-  end
+   @user = current_user
+   @artworks = Artwork.all
+ end
 
-  def new
-    @artwork = Artwork.new
-  end
+ def new
+  @artwork = Artwork.new
+end
 
-  def create
-    @artwork = Artwork.new(artwork_params)
-    if @artwork.save
-      redirect_to artwork_path(@artwork)
-    else
-      render :new
-    end
+def create
+  @artwork = Artwork.new(artwork_params)
+  if @artwork.save
+    redirect_to artwork_path(@artwork)
+  else
+    render :new
   end
+end
 
-  def show
-    @artwork = Artwork.find(params[:id])
+def show
+  @artwork = Artwork.find(params[:id])
+end
+
+def edit
+  @artwork = Artwork.find(params[:id])
+end
+
+def update
+  @artwork = Artwork.find(params[:id])
+  if @artwork.update(artwork_params)
+    redirect_to artwork_path(@artwork)
+  else
+    render :edit
   end
+end
 
-  def edit
-    @artwork = Artwork.find(params[:id])
-  end
+def destroy
+  @artwork = Artwork.find(params[:id])
+  @artwork.destroy
+  redirect_to artworks_path
+end
 
-  def update
-    @artwork = Artwork.find(params[:id])
-    if @artwork.update(artwork_params)
-      redirect_to artwork_path(@artwork)
-    else
-      render :edit
-    end
-  end
+def search
 
-  def destroy
-    @artwork = Artwork.find(params[:id])
-    @artwork.destroy
-    redirect_to artworks_path
-  end
+     @result = [ ]  #shows the results from the filter
 
-  def search
-    @result = [ ]  #shows the results from the filter
-     Artwork.all.map do |art|
-      #
-      booked = false
-      art.appointments.each do |appointment|
-        (appointment.start_date.to_date..appointment.end_date.to_date).each do |date|
-          booked = true if date.between?(Date.today   , Date.today + 50  )
+      @search = Artwork.where(category: params[:category], location: params[:city])
+      date = params[:date_range]
+      date = date.split(%r{\s*-\s*})
+      from = date[0].to_s
+      to = date[1].to_s
+      @from_request = Date.strptime(from, '%m/%d/%Y')
+      @to_request = Date.strptime(to, '%m/%d/%Y')
+
+      @search.all.map do |art|  #execute for all artwork instances
+
+       booked = false  #defaults all artwork to 'unbooked'
+            art.appointments.each do |appointment|
+              (appointment.start_date.to_date..appointment.end_date.to_date).each do |date|
+                booked = true if date.between?(@from_request   , @to_request  )
+              end
+            end
+         unless booked
+           artwork = {}
+           artwork[:id] = art.id
+           artwork[:title] = art.title
+           artwork[:photo] = art.photo
+           artwork[:user_id] = art.user_id
+           @result << artwork
         end
-      end
-      #
-      unless booked
-        artwork = {}
-        artwork[:title] = art.title
-        artwork[:photo] = art.photo
-        @result << artwork
-      end
 
-    end
+      end
+#redirect_to search_path
   end
+
+
+
+#     # ################ Below calculates city match ##################
+#     #  art.location.each do |location|
+#     #   booked = true if location == @user.location_request
+
+#     # end
+
+#     # ################ Below calculates Art Type Match ##################
+#     # art.category.each do |category|
+#     #  booked = true if category == @user.category_request
+#     # end
+
+#     ################ Below calculates date match ##################
+#     art.appointments.each do |appointment|
+#       (appointment.start_date.to_date..appointment.end_date.to_date).each do |date|
+#         booked = true if date.between?(@user.start_request, @user.end_request )
+#       end
+# ################### Original Code from Ruben#######################
+
+
+
+#       #
+#       unless booked
+#         artwork = {}
+#         artwork[:title] = art.title
+#         artwork[:photo] = art.photo
+#         @result << artwork
+#       end
+
+
+
 
   private
 
